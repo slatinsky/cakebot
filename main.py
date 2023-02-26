8ab06635261ada6e2d7ca8b86c5b789b2401530b
Schroti <schrotihd@gmail.com>
2023-02-26 13:59:40 +0100

    Cleanup main.py

--- main.py
+++ main.py
@@ -11,272 +11,213 @@ from InventoryImporter import InventoryImporter
 nest_asyncio.apply()
 
 if len(Utils.ALLOWED_CHANNEL_IDS) == 0:
-	print("No allowed channel IDs found in config.ini")
-	exit()
+    print("No allowed channel IDs found in config.ini")
+    exit()
 
 
 # verify if API_KEY is valid
 def verify_api_key():
-	r = requests.get('https://api.hypixel.net/player?key=' + Utils.API_KEY + '&uuid=' + Utils.SLADA_UUID)
-	if r.status_code == 200:
-		return True
-	else:
-		return False
+    r = requests.get('https://api.hypixel.net/player?key=' + Utils.API_KEY + '&uuid=' + Utils.SLADA_UUID)
+    if r.status_code == 200:
+        return True
+    else:
+        return False
 
 
 if not verify_api_key():
-	print("Invalid hypixel api_key")
-	exit()
+    print("Invalid hypixel api_key")
+    exit()
 else:
-	print("Hypixel api_key is valid")
-
+    print("Hypixel api_key is valid")
 
 
 def get_commit_hash():
-	return os.popen('git rev-parse --short HEAD').read().strip()
+    return os.popen('git rev-parse --short HEAD').read().strip()
+
 
 def get_commit_time():
-	return os.popen('git show -s --format="%ci"').read().strip()
+    return os.popen('git show -s --format="%ci"').read().strip()
+
 
 def get_commit_index():
-	count = 100 + int(os.popen('git rev-list --count HEAD').read().strip())
-	if git_unsaved_changes():
-		return count + 1
-	return count
+    count = 100 + int(os.popen('git rev-list --count HEAD').read().strip())
+    if git_unsaved_changes():
+        return count + 1
+    return count
+
 
 def git_unsaved_changes():
-	return len(os.popen('git diff-files').read().strip()) > 0
+    return len(os.popen('git diff-files').read().strip()) > 0
+
 
 def git_are_changes_ready_to_commit():
-	return os.system('git diff-index --quiet HEAD --') == 0
+    return os.system('git diff-index --quiet HEAD --') == 0
+
 
 def git_get_commit_messages():
-	changes = os.popen('git log --pretty=format:"%s"').read().strip().split('\n')
-	for i in range(len(changes)):
-		changes[i] = f"v{len(changes) - i + 100}: {changes[i]}"
+    changes = os.popen('git log --pretty=format:"%s"').read().strip().split('\n')
+    for i in range(len(changes)):
+        changes[i] = f"v{len(changes) - i + 100}: {changes[i]}"
 
-	changes = '\n'.join(changes)
-	return changes
+    changes = '\n'.join(changes)
+    return changes
+
 
 if git_unsaved_changes():
-	BETA = " BETA"
+    BETA = " BETA"
 else:
-	BETA = ""
+    BETA = ""
 VERSION_STRING = f"Bot version{BETA} {get_commit_index()} ({get_commit_hash()}, {get_commit_time()})"
 
 print(VERSION_STRING)
 
-# def get_commit_date():
-# 	return get_commit_time().split(' ')[0]
-
 # make dir "auction" if not exists
 if not os.path.exists('auction'):
-	os.makedirs('auction')
+    os.makedirs('auction')
 
-
-
-
-
-# print(uuid_add_dashes(SLADA_UUID))
-# download_auctions()
 cakes_obj = Cakes()
-# cakes_obj.analyze_undercuts(None, "Slada")
-# cakes_obj.analyze_undercuts(None, "P0tchi")
-# cakes_obj.analyze_undercuts(None, "SkillGrind")
-# exit()
-# cakes_obj.analyze_bin_prices(None, None)
-# cakes_obj.analyze_bin_prices(None, "cillian96470")
-#
-# cakes_obj.auctions_ending_soon("cillian96470")
-# exit(0)
-# cakes_obj.analyze_bin_prices()
-# print(cakes_obj.auctions_ending_soon())
-# print(cakes_obj.auctions_ending_soon("Slada"))
-# print(cakes_obj.auctions_ending_soon("Brumby98"))
-# cakes_obj.top(None)
-# exit(0)
-
-
-# print(InventoryImporter().offer_cakes("P0tchi"))
-# print(InventoryImporter().offer_cakes("SkillGrind"))
-# print(InventoryImporter().offer_cakes("Brumby98"))
-# print(InventoryImporter().offer_cakes("cillian96470"))
-# print(InventoryImporter().offer_cakes("Slada"))
-#
-
-# print("out:", InventoryImporter().offer_cakes("taftaf1"))
-# exit(0)
-
 bot = commands.Bot(command_prefix=Utils.COMMAND_PREFIX, help_command=None)
 
 
 @bot.event
 async def on_message(message):
-	print(message.content)
-	await bot.process_commands(message)
+    print(message.content)
+    await bot.process_commands(message)
 
 
 @bot.event
 async def on_ready():  # This function will be run by the discord library when the bot has logged in
-	print("Logged in as " + bot.user.name)
+    print("Logged in as " + bot.user.name)
 
-
-# @bot.command()
-# async def ping(ctx):
-# 	latency = bot.latency
-# 	await ctx.send(latency)
-
-# @bot.command()
-# async def h(ctx):
-# 	await ctx.send(f"Available commands:\n{Utils.COMMAND_PREFIX}uuid Slada - returns UUID")
 
 async def is_dm(ctx):
-	#print(ctx.author.id)
-	deny_author_ids = []  # copy author id from discord
+    # print(ctx.author.id)
+    deny_author_ids = []  # copy author id from discord
 
-	if ctx.author.id in deny_author_ids:
-		return True  # do not allow commands from these users
+    if ctx.author.id in deny_author_ids:
+        return True  # do not allow commands from these users
 
+    if ctx.message.channel.id not in Utils.ALLOWED_CHANNEL_IDS:
+        print("not in allowed channel")
+        return True  # not correct channel ID, ignore command
 
-	if ctx.message.channel.id not in Utils.ALLOWED_CHANNEL_IDS:
-		print("not in allowed channel")
-		return True  # not correct channel ID, ignore command
-
-	if ctx.guild is None:
-		await ctx.send("Don't be shy! Talk with me in bot-channel!")
-		return True
-	else:
-		return False
+    if ctx.guild is None:
+        await ctx.send("Don't be shy! Talk with me in bot-channel!")
+        return True
+    else:
+        return False
 
 
 @bot.command()
 async def col(ctx, mc_name):
-	if await is_dm(ctx):
-		return
-	msg = InventoryImporter().offer_cakes(mc_name)
-	if len(msg) > 1995:
-		await cakes_obj.split_and_send(ctx, msg.replace('```', ''))
-	else:
-		await ctx.send(msg)
-
+    if await is_dm(ctx):
+        return
+    msg = InventoryImporter().offer_cakes(mc_name)
+    if len(msg) > 1995:
+        await cakes_obj.split_and_send(ctx, msg.replace('```', ''))
+    else:
+        await ctx.send(msg)
 
 
 @bot.command()
 async def top(ctx):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	await ctx.send(cakes_obj.top(None))
+    await ctx.send(cakes_obj.top(None))
 
-
-# @bot.command()
-# async def uuid(ctx, mc_name):
-# 	mc_uuid = get_uuid_from_mc_name(mc_name)
-# 	await ctx.send(f"UUID: {mc_uuid}")
-
-# @bot.command()
-# async def hello(ctx):
-# 	await ctx.send("Hello everyone!")
-
-# @bot.command()
-# async def shutdown(ctx):
-# 	await ctx.send("Bye!")
 
 @bot.command()
 async def uc(ctx, mc_name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	if mc_name is not None:
-		await cakes_obj.analyze_undercuts(ctx, mc_name)
-	else:
-		await ctx.send(f"Invalid syntax, use {Utils.COMMAND_PREFIX}uc NAME")
+    if mc_name is not None:
+        await cakes_obj.analyze_undercuts(ctx, mc_name)
+    else:
+        await ctx.send(f"Invalid syntax, use {Utils.COMMAND_PREFIX}uc NAME")
+
 
 @bot.command()
 async def undercuts(ctx, mc_name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	if mc_name is not None:
-		await cakes_obj.analyze_undercuts(ctx, mc_name)
-	else:
-		await ctx.send(f"Invalid syntax, use {Utils.COMMAND_PREFIX}undercuts NAME")
-
+    if mc_name is not None:
+        await cakes_obj.analyze_undercuts(ctx, mc_name)
+    else:
+        await ctx.send(f"Invalid syntax, use {Utils.COMMAND_PREFIX}undercuts NAME")
 
 
 @bot.command()
 async def bins(ctx, mc_name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	await cakes_obj.analyze_bin_prices(ctx, mc_name)
+    await cakes_obj.analyze_bin_prices(ctx, mc_name)
 
-
-# await ctx.send(cakes_obj.analyze_bin_prices())
 
 @bot.command()
 async def soon(ctx):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	await cakes_obj.auctions_ending_soon(ctx)
+    await cakes_obj.auctions_ending_soon(ctx)
 
-
-# await ctx.send(cakes_obj.analyze_bin_prices())
 
 @bot.command()
 async def ah(ctx, name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	if name is not None:
-		await cakes_obj.auctions_ending_soon(ctx, name)
-	else:
-		await ctx.send(f"Invalid syntax, use {Utils.COMMAND_PREFIX}ah NAME")
+    if name is not None:
+        await cakes_obj.auctions_ending_soon(ctx, name)
+    else:
+        await ctx.send(f"Invalid syntax, use {Utils.COMMAND_PREFIX}ah NAME")
+
 
 @bot.command()
 async def version(ctx, name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	await ctx.send(VERSION_STRING)
+    await ctx.send(VERSION_STRING)
 
 
 @bot.command()
 async def tb(ctx, name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	if name is not None:
-		await cakes_obj.auctions_ending_soon(ctx, None, name)
-	else:
-		await ctx.send(f"Invalid syntax, use !bids NAME")
+    if name is not None:
+        await cakes_obj.auctions_ending_soon(ctx, None, name)
+    else:
+        await ctx.send(f"Invalid syntax, use !bids NAME")
 
 
 @bot.command()
 async def delcache(ctx, name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	# if mcnames.db exists, delete it
-	if os.path.exists("mcnames.db"):
-		Utils.delete_database()
-		if os.path.exists("mcnames.db"):
-			await ctx.send("Failed to delete cache (mcnames.db)")
-		else:
-			await ctx.send(f"Deleted mcnames.db")
-	else:
-		await ctx.send("Cache (mcnames.db) does not exist!")
-
+    # if mcnames.db exists, delete it
+    if os.path.exists("mcnames.db"):
+        Utils.delete_database()
+        if os.path.exists("mcnames.db"):
+            await ctx.send("Failed to delete cache (mcnames.db)")
+        else:
+            await ctx.send(f"Deleted mcnames.db")
+    else:
+        await ctx.send("Cache (mcnames.db) does not exist!")
 
 
 @bot.command()
 async def help(ctx):
-	if await is_dm(ctx):
-		return
-	print("printing help")
+    if await is_dm(ctx):
+        return
+    print("printing help")
 
-	help = f"""
+    help = f"""
 Deprecation warning:
 This cake bot is deprecated. That means, that I (Slada) am not playing the game anymore and I can't improve the bot. I will try to keep the bot online and do small bug fixes. I promise, that the bot will stay online at least till 2022-01-01.
 
@@ -324,15 +265,15 @@ Available commands:
 ```{Utils.COMMAND_PREFIX}delcache```- deletes name cache
 ```{Utils.COMMAND_PREFIX}changelog```- see bot changes
 """
-	await ctx.send(help)
+    await ctx.send(help)
 
 
 @bot.command()
 async def changelog(ctx, name=None):
-	if await is_dm(ctx):
-		return
+    if await is_dm(ctx):
+        return
 
-	await ctx.send(git_get_commit_messages())
+    await ctx.send(git_get_commit_messages())
 
 
 # Run the discord bot
