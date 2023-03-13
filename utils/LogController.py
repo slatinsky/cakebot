1607d0ba93cdad438859faabefe2ac946ebe203c
Schroti <schrotihd@gmail.com>
2023-03-13 15:49:20 +0100

    Only add log handlers once
    Messages got duplicated every time a log handler was added by instantiating logcontroller from another file

--- utils/LogController.py
+++ utils/LogController.py
@@ -13,22 +13,23 @@ class LogController:
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.setLevel(Config.LOG_LEVEL)
 
-        formatter = logging.Formatter(
-            "[%(levelname)s] %(asctime)s: %(message)s",
-            "%Y-%m-%d %H:%M:%S")
-        formatter.converter = time.gmtime
+        if not self.logger.hasHandlers():
+            formatter = logging.Formatter(
+                "[%(levelname)s] %(asctime)s: %(filename)s | %(message)s",
+                "%Y-%m-%d %H:%M:%S")
+            formatter.converter = time.gmtime
 
-        if not os.path.exists("logs"):
-            os.makedirs("logs")
+            if not os.path.exists("logs"):
+                os.makedirs("logs")
 
-        file_handler = RotatingFileHandler("logs/discord.log", maxBytes=Config.LOG_MAX_SIZE * 1000000,
-                                           backupCount=Config.LOG_MAX_FILES)
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
+            file_handler = RotatingFileHandler("logs/discord.log", maxBytes=Config.LOG_MAX_SIZE * 1000000,
+                                               backupCount=Config.LOG_MAX_FILES)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
 
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setFormatter(formatter)
-        self.logger.addHandler(console_handler)
+            console_handler = logging.StreamHandler(sys.stdout)
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
 
     def get_logger(self):
         return self.logger
